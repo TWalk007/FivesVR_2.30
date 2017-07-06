d40a587054fd2bad2ec063c32fa42b4dce4359ec
TWalk007 <walkertb03@gmail.com>
2017-07-06 16:08:28 -0400

    Repository Initiation

---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+
+	void Start () {
+		Scene scene = SceneManager.GetActiveScene ();
+
+		if (scene.name == "00_Splash") {
+			Invoke ("LoadNextScene", autoLoadNextLevelAfter);
+		}
+
+	}
+
+
+	public void LoadNextScene (){
+		int currentIndex =  UnityEngine.SceneManagement.SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex + 1);
+	}
+
+	public void LoadPreviousScene (){
+		int currentIndex =  UnityEngine.SceneManagement.SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex - 1);
+	}
+
+	public void LoadLevel (string name){
+		SceneManager.LoadScene (name);
+	}
+
+
+	public void ResetScene (){
+		int currentIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex);
+	}
+
+	public void Quit()	{
+		Application.Quit ();
+		print ("Game will quit.");
+	}
+
+}
