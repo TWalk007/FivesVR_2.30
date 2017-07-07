76f85802a7ebfcf678ee85c40190bd20b717d2ca
TWalk007 <walkertb03@gmail.com>
2017-07-07 16:32:29 -0400

    End of day check in

---
+++ Assets/_Scripts/HotspotShaderAdjustment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotspotShaderAdjustment : MonoBehaviour {
+
+	public bool colorCrossFade = false;
+	public Color colorStart = Color.white;
+	public Color colorEnd = Color.magenta;
+	public float duration = 1.0f;
+
+	private Renderer rend;
+
+
+	void Start () {
+		rend = this.GetComponent<Renderer> ();
+	}
+	
+	void Update () {
+		if (colorCrossFade) {
+			CrossFade ();
+		}
+	}
+
+	private void CrossFade(){
+		float lerp = Mathf.PingPong (Time.time, duration) / duration;
+		rend.material.color = Color.Lerp (colorStart, colorEnd, lerp);
+	}
+}
---
+++ Assets/_Scripts/ToolChangerHotspot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolChangerHotspot : MonoBehaviour {
+
+	public float gazeActivationTime = 1.25f;
+	public ExploreController exploreController;
+
+	private float timeElapsed;
+	private bool isEntered = false;
+
+	void Update () {
+		if (isEntered) {
+			ActivateTC ();
+		}
+	}
+
+	void ActivateTC(){
+		timeElapsed += Time.deltaTime;
+		if (timeElapsed >= gazeActivationTime) {
+			timeElapsed = 0;
+			exploreController.myState = ExploreController.States.toolChanger;
+		}
+	}
+
+	public void OnPointerEnter (){
+		isEntered = true;
+	}
+
+	public void OnPointerExit (){
+		isEntered = false;
+		timeElapsed = 0;
+	}
+
+}
