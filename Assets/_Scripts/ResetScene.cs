b50d634af1de71446d0620846aec46128bf1ef6f
TWalk007 <walkertb03@gmail.com>
2017-07-10 11:14:43 -0400

    Pre-test Check-in

---
+++ Assets/_Scripts/ResetScene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetScene : MonoBehaviour {
+
+	public float gazeActivationTime = 1.25f;
+
+	private LevelManager levelManager;
+
+	private float timeElapsed;
+	private bool resetScene = false;
+
+
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+
+	}
+
+	void Update () {
+		if (resetScene){
+			timeElapsed += Time.deltaTime;
+
+			if (timeElapsed >= gazeActivationTime) {
+				timeElapsed = 0;
+				levelManager.ResetScene ();
+				resetScene = false;
+			}
+		} else {
+			timeElapsed = 0;
+		}
+	}
+
+	public void OnPointerEnter (){
+		resetScene = true;
+	}
+
+	public void OnPointerExit (){
+		resetScene = false;
+		timeElapsed = 0;
+	}
+
+}
---
+++ Assets/_Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToMainMenu : MonoBehaviour {
+
+	public float gazeActivationTime = 1.25f;
+
+	private LevelManager levelManager;
+
+	private float timeElapsed;
+	private bool returnToMainMenu = false;
+
+
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+
+	}
+
+	void Update () {
+		if (returnToMainMenu){
+			timeElapsed += Time.deltaTime;
+
+			if (timeElapsed >= gazeActivationTime) {
+				timeElapsed = 0;
+				levelManager.LoadLevel ("01_Main");
+				returnToMainMenu = false;
+			}
+		} else {
+			timeElapsed = 0;
+		}
+	}
+
+	public void OnPointerEnter (){
+		returnToMainMenu = true;
+	}
+
+	public void OnPointerExit (){
+		returnToMainMenu = false;
+		timeElapsed = 0;
+	}
+
+}
